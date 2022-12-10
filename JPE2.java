43ecfd2239b8dc824d99787aaa3b5dffdc4e419a
Anushka01114 <119841504+Anushka01114@users.noreply.github.com>
2022-12-10 09:47:28 +0530

    Update JPE2.java

--- JPE2.java
+++ JPE2.java
@@ -1,13 +1,136 @@
-class clock()
+public class Clock
 {
-  int hours. min, sec;  
+    private int hr; 
+    private int min; 
+    private int sec; 
+
+    public Clock(int hours, int minutes, int seconds)
+    {
+	      setTime(hours, minutes, seconds);
+    }
+          //Default constructor
+	        //Postcondition: hr = 0; min = 0; sec = 0
+    public Clock()
+    {
+        setTime(0, 0, 0);
+    }
+
+    public void setTime(int hours, int minutes, int seconds)
+    {
+	      if(0 <= hours && hours < 24)
+		       hr = hours;
+	      else
+		       hr = 0;
+
+        if(0 <= minutes && minutes < 60)
+		       min = minutes;
+	      else
+		       min = 0;
+
+	      if(0 <= seconds && seconds < 60)
+		       sec = seconds;
+	      else
+	         sec = 0;
+    }
+
+    public int getHours()
+    {
+	        return hr;
+    }
+    public int getMinutes()
+    {
+	        return min;
+    }
+
+    public int getSeconds()
+    {
+	        return sec;
+    }
+
+    public void printTime()
+    {
+ 	        if(hr < 10)
+	           System.out.print("0");
+          System.out.print(hr + ":");
+
+	        if(min < 10)
+	           System.out.print("0");
+	        System.out.print(min + ":");
+
+	        if(sec < 10)
+	           System.out.print("0");
+	        System.out.print(sec);
+    }
+
+    public void incrementSeconds()
+    {
+          sec++;
+	        if(sec > 59)
+	        {
+	           sec = 0;
+	           incrementMinutes(); //increment minutes
+	        }
+    }
   
-  class clock()
-  {
-    
-  }
-  
-  
-  
+  //is reset to 00:00:53
+    public void incrementMinutes()
+    {
+	        min++;
+	        if(min > 59)
+	        {
+	           min = 0;
+	           incrementHours(); //increment hours
+	        }
+    }
+ public void incrementHours()
+    {
+	      hr++;
+	      if(hr > 23)
+ 	         hr = 0;
+    }
+
+    public boolean equals(Clock otherClock)
+	  {
+        return(hr == otherClock.hr
+ 	             && min == otherClock.min
+               && sec == otherClock.sec);
+    }
+
+    public void makeCopy(Clock otherClock)
+	  {
+        hr = otherClock.hr;
+        min = otherClock.min;
+        sec = otherClock.sec;
+    }
+
+    public Clock getCopy()
+    {
+	      Clock temp = new Clock();
+
+	      temp.hr = hr;
+	      temp.min = min;
+	      temp.sec = sec;
+
+	      return temp;
+    }
+
+    public String toString()
+    {
+        String str = "";
+
+        if(hr < 10)
+	         str = "0";
+        str = str + hr + ":";
+
+        if(min < 10)
+	         str = str + "0" ;
+        str = str + min + ":";
+
+        if(sec < 10)
+	         str = str + "0";
+        str = str + sec;
+
+        return str;
+    }
 }
 
