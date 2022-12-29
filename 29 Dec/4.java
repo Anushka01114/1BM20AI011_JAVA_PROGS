7de312bdfac8805542ec2214f743448d9ceec205
Anushka01114 <119841504+Anushka01114@users.noreply.github.com>
2022-12-29 20:20:24 +0530

    Create 4.java

---
+++ 29 Dec/4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ReverseOddString{
+ public static void main(String[] args) {
+    String s;
+    int j=0;
+    System.out.println("Enter a string");
+    Scanner so=new Scanner(System.in);
+    s= so.nextLine();
+   System.out.println("\nOriginal text: " + s);
+   System.out.println("\nReversed text of the words with odd length:" + test(s));
+ }
+ public static String test(String str) {
+   String[] str_words = str.split(" ");
+   for (int i = 0; i < str_words.length; i++) {
+     if (str_words[i].length() % 2 != 0) {
+       StringBuilder reverser = new StringBuilder(str_words[i]);
+       str_words[i] = reverser.reverse().toString();
+     }
+   }
+   return String.join(" ", str_words);
+ }
+}
